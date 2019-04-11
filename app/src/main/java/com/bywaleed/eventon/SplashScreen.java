c968562113d7c37f569d7bd774ac7ca344206d4e
Waleed <waleedi825@gmail.com>
2019-04-11 14:33:09 +0100

    Splash Screen Created

---
+++ app/src/main/java/com/bywaleed/eventon/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.bywaleed.eventon;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        // Load Splash Screen
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, 3000);
+    }
+}
